7566b3b7eb6b3dc4b6023eea496df5a8dcf3961e
Saurav Kumar <97390971+Milkyy-way@users.noreply.github.com>
2024-11-23 01:58:41 -0500

    Update scatterplot.problem.js

--- js/scatterplot.problem.js
+++ js/scatterplot.problem.js
@@ -1,184 +1,187 @@
-function scatter_plot(
-    data,
-    ax,
-    title = "",
-    xCol = "",
-    yCol = "",
-    rCol = "",
-    legend = [],
-    colorCol = "",
-    margin = 50
-) {
-    const X = data.map(d => d[xCol]);
-    const Y = data.map(d => d[yCol]);
-    const R = data.map(d => d[rCol]);
-    const colorCategories = [...new Set(data.map(d => d[colorCol]))]; 
-
-    const color = d3
-        .scaleOrdinal()
+function scatter_plot(data,
+                                 ax,
+                                 title="",
+                                 xCol="",
+                                 yCol="",
+                                 rCol="",
+                                 legend=[],
+                                 colorCol="",
+                                 margin = 50)
+{
+    const X = data.map(d=>d[xCol])
+    const Y = data.map(d=>d[yCol])
+    const R = data.map(d=>d[rCol])
+    const colorCategories =  [... new Set(data.map(d=>d[colorCol]))] 
+        const color = d3.scaleOrdinal()
         .domain(colorCategories)
-        .range(d3.schemeTableau10); 
+        .range(d3.schemeTableau10) 
 
     const xExtent = d3.extent(X, d => +d);
     const yExtent = d3.extent(Y, d => +d);
 
     const xMargin = (xExtent[1] - xExtent[0]) * 0.05; 
-    const yMargin = (yExtent[1] - yExtent[0]) * 0.05;
+    const yMargin = (yExtent[1] - yExtent[0]) * 0.05; 
 
-    const xScale = d3
-        .scaleLinear()
+    const xScale = d3.scaleLinear()
         .domain([xExtent[0] - xMargin, xExtent[1] + xMargin])
         .range([margin, 1000 - margin]);
 
-    const yScale = d3
-        .scaleLinear()
+    const yScale = d3.scaleLinear()
         .domain([yExtent[0] - yMargin, yExtent[1] + yMargin])
         .range([1000 - margin, margin]);
 
-    const rScale = d3
-        .scaleSqrt()
-        .domain(d3.extent(R, d => +d))
-        .range([4, 12]);
+    const rScale= d3.scaleSqrt().domain(d3.extent(R, d=>+d))
+                                .range([4,12])
+    const Fig = d3.select(`${ax}`)
 
-    const Fig = d3.select(`${ax}`);
-
-    
-    Fig.selectAll(".markers")
+    Fig.selectAll('.markers')
         .data(data)
-        .join("g")
-        .attr("transform", d => `translate(${xScale(d[xCol])}, ${yScale(d[yCol])})`)
-        .append("circle")
-        .attr("class", (d, i) => `cls_${i} ${d[colorCol]}`)
-        .attr("id", (d, i) => `id_${i} ${d[colorCol]}`)
-        .attr("r", d => rScale(d[rCol]))
-        .attr("fill", d => color(d[colorCol]));
+        .join('g')
+        .attr('transform', d=>`translate(${xScale(d[xCol])}, ${yScale(d[yCol])})`)
+        .append('circle')
+        .attr("class", (d,i)=>`cls_${i} ${d[colorCol]}`)
+        .attr("id", (d,i)=>`id_${i} ${d[colorCol]}`)
+        .attr("r",d=>rScale(d[rCol]))
+        .attr("fill", d=>{
+           return color(d[colorCol])
+        })
 
-    
-    Fig.append("g")
-        .attr("class", "axis")
-        .attr("transform", `translate(0,${1000 - margin})`)
-        .call(d3.axisBottom(xScale).ticks(4));
-
-    Fig.append("g")
-        .attr("class", "axis")
-        .attr("transform", `translate(${margin},0)`)
-        .call(d3.axisLeft(yScale).ticks(4));
-
-    
-    Fig.append("g")
-        .attr("transform", `translate(500,${1000 - 10})`)
-        .append("text")
-        .attr("class", "label")
-        .text(xCol)
-        .attr("fill", "black");
-
-    Fig.append("g")
-        .attr("transform", `translate(35,500) rotate(270)`)
-        .append("text")
-        .attr("class", "label")
-        .text(yCol)
-        .attr("fill", "black");
-
- 
-    Fig.append("text")
-        .attr("x", 500)
-        .attr("y", 80)
-        .attr("text-anchor", "middle")
-        .text(title)
-        .attr("class", "title")
-        .attr("fill", "black");
 
    
+    const x_axis = d3.axisBottom(xScale).ticks(4)
+    const y_axis = d3.axisLeft(yScale).ticks(4)
+   
+    Fig.append("g").attr("class","axis")
+        .attr("transform", `translate(${0},${1000-margin})`)
+        .call(x_axis)
+   
+    Fig.append("g").attr("class","axis")
+        .attr("transform", `translate(${margin},${0})`)
+        .call(y_axis)
+    
+    Fig.append("g").attr("class","label")
+        .attr("transform", `translate(${500},${1000-10})`)
+        .append("text")
+        .attr("class","label")
+        .text(xCol)
+        .attr("fill", "black")
+
+    Fig.append("g")
+        .attr("transform", `translate(${35},${500}) rotate(270)`)
+        .append("text")
+        .attr("class","label")
+        .text(yCol)
+        .attr("fill", "black")
+
+
+    
+    Fig.append('text')
+        .attr('x',500)
+        .attr('y',80)
+        .attr("text-anchor","middle")
+        .text(title)
+        .attr("class","title")
+        .attr("fill", "black")
+ 
     const brush = d3
         .brush()
-        .on("start", brushStart)
+        .on("start",  brushStart)
         .on("brush end", brushed)
         .extent([
             [margin, margin],
-            [1000 - margin, 1000 - margin]
+            [1000-margin,1000-margin]
         ]);
 
     Fig.call(brush);
+    
 
     function brushStart() {
-        d3.selectAll("circle").classed("selected", false); 
+        
+        if (d3.brushSelection(this)[0][0] === d3.brushSelection(this)[1][0]) {
+
+            
+            d3.selectAll("circle").classed("selected", false);
+
+        }
     }
 
-    function brushed({ selection }) {
-        if (!selection) return; 
+    function brushed() {
+        
+        let selected_coordinates = d3.brushSelection(this); 
+        if (!selected_coordinates) return; 
 
-        const [[x0, y0], [x1, y1]] = selection;
-
-        const X1 = xScale.invert(x0);
-        const X2 = xScale.invert(x1);
-        const Y1 = yScale.invert(y1);
-        const Y2 = yScale.invert(y0);
+        const X1 = xScale.invert(selected_coordinates[0][0]);
+        const X2 = xScale.invert(selected_coordinates[1][0]);
+        const Y1 = yScale.invert(selected_coordinates[0][1]);
+        const Y2 = yScale.invert(selected_coordinates[1][1]);
 
         
-        d3.selectAll("circle").classed("selected", d => {
-            return (
-                +d[xCol] >= X1 &&
-                +d[xCol] <= X2 &&
-                +d[yCol] <= Y1 &&
-                +d[yCol] >= Y2
-            );
+        d3.selectAll("circle").classed("selected", (d, i) => {
+            if (+d[xCol] >= X1 && +d[xCol] <= X2 && +d[yCol] <= Y1 && +d[yCol] >= Y2) {
+                return true;
+            }
+            return false;
         });
+
     }
 
-  
-    const legendContainer = Fig.append("g")
+    const legendContainer = Fig
+        .append("g")
         .attr("transform", `translate(${800},${margin})`)
-        .attr("class", "marginContainer");
+        .attr("class","marginContainer")
+    if(legend.length===0){legend=colorCategories}
 
-    if (legend.length === 0) legend = colorCategories;
-
-    const legendItems = legendContainer
-        .selectAll("legends")
+    const legends_items = legendContainer.selectAll("legends")
         .data(legend)
         .join("g")
-        .attr("transform", (d, i) => `translate(0,${i * 45})`);
+        .attr("transform",(d,i)=>`translate(${0},${i*45})`)
 
-    legendItems
-        .append("rect")
-        .attr("fill", d => color(d))
-        .attr("width", "40")
-        .attr("height", "40")
-        .attr("class", d => d);
 
-   
-    let selectedCategories = [];
-
-    legendItems.on("click", (event, d) => {
-        const isSelected = selectedCategories.includes(d);
-
-        if (isSelected) {
-            selectedCategories = selectedCategories.filter(cat => cat !== d);
-        } else {
-            selectedCategories.push(d);
-        }
+    legends_items.append("rect")
+        .attr("fill",d=>color(d))
+        .attr("width","40")
+        .attr("height","40")
+        .attr("class",d=>d)
 
         
-        d3.selectAll("circle")
-            .transition()
-            .duration(200)
-            .style("opacity", c => {
-                return selectedCategories.length === 0 ||
-                    selectedCategories.includes(c[colorCol])
-                    ? 1
-                    : 0.2;
-            });
+        let selectedCountries = [];
 
-        
-        d3.selectAll("circle").classed("selected", c =>
-            selectedCategories.includes(c[colorCol])
-        );
-    });
+        legends_items.on("click", (event, d) => {
+            
+            const isSelected = selectedCountries.includes(d);
 
-    legendItems
+            if (isSelected) {
+                
+                selectedCountries = selectedCountries.filter(country => country !== d);
+            } else {
+                
+                selectedCountries.push(d);
+            }
+
+            
+            d3.selectAll("circle")
+                .transition() 
+                .duration(200) 
+                .style("opacity", (c) => {
+                    
+                    return selectedCountries.includes(c[colorCol]) ? 0.2 : 1;
+                });
+
+            
+            d3.selectAll("circle")
+                .classed("selected", c => selectedCountries.includes(c[colorCol]));
+        });
+
+
+
+
+    legends_items
         .append("text")
-        .text(d => d)
-        .attr("dx", 45)
-        .attr("dy", 25)
-        .attr("class", "legend")
-        .attr("fill", "black");
+        .text(d=>d)
+        .attr("dx",45)
+        .attr("dy",25)
+        .attr("class","legend")
+        .attr("fill", "black")
+
+
 }
